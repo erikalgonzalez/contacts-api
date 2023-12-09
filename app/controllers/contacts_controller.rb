93ff209a4cab6edcf4ed73aafd7df9662b628bfc
Erika Gonzalez <erikagonzz01@gmail.com>
2023-12-08 19:28:03 -0500

    show data on first contact

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,12 @@
+class ContactsController < ApplicationController
+  def show_contacts
+    contact = Contact.find_by(id: 1)
+    render json:{
+      id: contact.id,
+      first_name: contact.first_name,
+      last_name: contact.last_name,
+      email: contact.email,
+      phone_number: contact.phone_number,
+    }
+  end
+end
